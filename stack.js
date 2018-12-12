655dc9ae2416da1b089c169035d460180e78d521
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2018-12-12 11:43:52 -0800

    mvp stack

--- stack.js
+++ stack.js
@@ -1,17 +1,26 @@
-class Stack () {
+'use strict'
+require('./linked_list.js');
+
+class Stack {
+  constructor(store) {
+    this.list = store;
+  }
+  
   push(val) {
-    
+    this.list.add(val);
   }
   
   pop() {
-    
+    let node = this.list.head;
+    this.list.head = node.next;
+    return node.value;
   }
   
   peek() {
-    
+    this.list.head.value;
   }
   
   isEmpty() {
-    
+    return true if this.list.head === null;
   }
 }
