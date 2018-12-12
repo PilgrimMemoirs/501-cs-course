b7494237efc3c0a00a369e446273cd26e608d81a
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2018-12-12 15:59:51 -0800

    permutable palindrom pseudocode

---
+++ perm_palindrom.js
@@ -0,0 +1,20 @@
+permPalindrom = (s) => {
+  // s input to array
+  // create letters hash (letters are keys, num of that letter in string is value)
+  
+  //iterate array, letter adds 1 to the value of that key in the letters hash
+  // for ( let i = 0; )
+  
+  
+  // conditional to check length of hash.
+    // if even
+      // return true if all values are even
+      // return false if not all values are even
+      
+    //if odd
+      // create odd_counter
+      // return false as soon as more than 1 odd is detected
+      // return true is only one odd
+}
+
+console.log(permPalindrom('hey'));
