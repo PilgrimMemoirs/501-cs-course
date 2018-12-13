3df32b33a9caa82c6a7a9df5ca59337cea22cb93
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2018-12-13 14:13:58 -0800

    binary tree add function

--- binary_tree.js
+++ binary_tree.js
@@ -1,120 +1,262 @@
+// 'use strict';
+// 
+// class Node {
+//   constructor(val, next) {
+//     this.value = val;
+//     this.next = next;
+//   }
+// }
+// 
+// 
+// class LinkedList {
+//   constructor() {
+//     this.head = null;
+//     this.tail = null;
+//   }
+// 
+//   add(val) {
+//     let nn = new Node(val);
+//     nn.next = this.head;
+//     this.head = nn;
+//     if ( this.head.next == null) {
+//       this.tail = this.head;
+//     }
+//   }
+// 
+//   traverse(fun) {
+//     let current = this.head;
+//     while (current) {
+//       fun(current.value);
+//       current = current.next;
+//     }
+//   }
+// 
+//   // removes all nodes that have the input value
+//   remove(val) {
+//     let current = this.head;
+// 
+//     if (val == current.value) {
+//       this.head = current.next;
+//       if (!this.head) return
+//     }
+// 
+//     while(current.next.next) {
+//       if (val == current.next.value) {
+//         current.next = current.next.next;
+// 
+//       }
+//       current = current.next;
+//     } 
+//   }
+// }
+// 
+// 
+// class Queue() {
+//   constructor(list){
+//     this.list = list;
+//   }
+// 
+//   //adds new elements to tail (first in)
+//   enqueue (val) {
+//     let nn = new Node(val);
+//     this.list.tail.next = nn;
+//     this.list.tail = nn;
+//   }
+// 
+//   //removes from tail (first out)
+//   dequeue () {
+//     currentHead = this.list.head;
+//     this.list.head = currentHead.next;
+//     return currentHead.value;
+//   }
+// 
+//   peek () {
+//     this.list.head.value;
+//   }
+// 
+//   isEmpty () {
+//     return !this.list.head.value;
+//   }
+// }
+// 
+// 
+// 
+// ////////////////////////////////////////////////////////////////////////////////
+// ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// 
+// 
+// 
+// 
+// class BinaryTreeNode {
+//   constructor(val = null) {
+//     this.left = null;
+//     this.right = null;
+//     this.value = val;
+//   }
+// 
+//   preOrderTraverse(cb) {
+//     cb(this.value); // cb called first
+//     if (this.left){ this.left.preOrderTraverse(cb) };
+//     if (this.right){  this.right.preOrderTraverse(cb) };
+//      // this returns undefined since there's no explicit return
+//   }
+// 
+//   inOrderTraverse(cb) {
+//     if (this.left){ this.left.inOrderTraverse(cb) };
+//     cb(this.value); //  cb called in between 
+//     if (this.right){  this.right.inOrderTraverse(cb) };
+//     // this returns undefined since there's no explicit return
+//   }
+// 
+//   postOrderTraverse(cb) {
+//     if (this.left){ this.left.postOrderTraverse(cb) };
+//     if (this.right){  this.right.postOrderTraverse(cb) };
+// 
+//      cb(this.value); // cb called at end
+//      // this returns undefined since there's no explicit return
+//   }
+// }
+// 
+// class BinaryTree {
+//   constructor() {
+//     this.root = new BinaryTreeNode();
+//   }
+// 
+//   breadthFirst(cb) {
+//     list = new LinkedList;
+//     let q = new Queue(list);
+//     q.enqueue(this.root);
+// 
+//     while(!q.isEmpty()) {
+//       let n = q.dequeue();
+//       cb(n);
+//       if (n.left) q.enqueue(n.left);
+//       if (n.right) q.enqueue(n.right);
+//     }
+//   }// breadthFirst
+// }// BinaryTree
+// 
+// let b = new BinaryTree();
+// 
+// b.root.value = 11;
+// 
+// ///////////
+// // Manually set nodes
+// //////////
+// 
+// b.root.left = new BinaryTreeNode(98);
+// b.root.right = new BinaryTreeNode(77);
+// 
+// b.root.left.right = new BinaryTreeNode(1);
+// b.root.left.left = new BinaryTreeNode(9);
+// 
+// b.root.left.left.left = new BinaryTreeNode(32398);
+// b.root.left.left.right = new BinaryTreeNode(32334234298);
+// 
+// 
+// 
+// // console.log(b.root);
+// // b.root.preOrderTraverse(console.log);
+// 
+// b.breadthFirst(console.log);
+
+
+
+
 'use strict';
 
 class Node {
-  constructor(val, next) {
-    this.value = val;
+  constructor(value, next) {
+    this.value = value;
     this.next = next;
   }
 }
 
-
 class LinkedList {
   constructor() {
     this.head = null;
-    this.tail = null;
   }
-  
-  add(val) {
-    let nn = new Node(val);
-    nn.next = this.head;
-    this.head = nn;
-    if ( this.head.next == null) {
-      this.tail = this.head;
-    }
+
+  add(value) {
+    let newNode = new Node(value);
+    newNode.next = this.head;
+    this.head = newNode;
   }
-  
-  traverse(fun) {
+
+  traverse(proc) {
     let current = this.head;
     while (current) {
-      fun(current.value);
+      proc(current);
       current = current.next;
     }
   }
-  
-  // removes all nodes that have the input value
-  remove(val) {
-    let current = this.head;
-  
-    if (val == current.value) {
-      this.head = current.next;
-      if (!this.head) return
+}
+
+class Queue {
+  constructor(line) {
+    this.list = line;
+  }
+
+  enqueue(val) {
+    let node = new Node(val);
+    if (!this.list.head) {
+      this.list.head = node;
+      this.list.tail = node;
+    } else {
+      this.list.tail.next = node;
+      this.list.tail = node;
     }
-    
-    while(current.next.next) {
-      if (val == current.next.value) {
-        current.next = current.next.next;
-        
-      }
-      current = current.next;
-    } 
+  }
+
+  dequeue() {
+    let remove = this.list.head;
+    this.list.head = remove.next;
+    return remove.value;
+  }
+
+  peek() {
+    return this.list.head.value;
+  }
+
+  isEmpty() {
+    return !this.list.head;
   }
 }
-
-
-class Queue() {
-  constructor(list){
-    this.list = list;
-  }
-  
-  //adds new elements to tail (first in)
-  enqueue (val) {
-    let nn = new Node(val);
-    this.list.tail.next = nn;
-    this.list.tail = nn;
-  }
-  
-  //removes from tail (first out)
-  dequeue () {
-    currentHead = this.list.head;
-    this.list.head = currentHead.next;
-    return currentHead.value;
-  }
-  
-  peek () {
-    this.list.head.value;
-  }
-  
-  isEmpty () {
-    return !this.list.head.value;
-  }
-}
-
-
-
-////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
 
 
 class BinaryTreeNode {
-  constructor(val = null) {
+  constructor(value = null) {
     this.left = null;
     this.right = null;
-    this.value = val;
+    this.value = value;
   }
   
-  preOrderTraverse(cb) {
-    cb(this.value); // cb called first
-    if (this.left){ this.left.preOrderTraverse(cb) };
-    if (this.right){  this.right.preOrderTraverse(cb) };
-     // this returns undefined since there's no explicit return
-  }
-  
-  inOrderTraverse(cb) {
-    if (this.left){ this.left.inOrderTraverse(cb) };
-    cb(this.value); //  cb called in between 
-    if (this.right){  this.right.inOrderTraverse(cb) };
-    // this returns undefined since there's no explicit return
+  add(val) {
+    if (this.val > val) {
+      if (!this.left) this.left.add(val);
+      else this.left = new BinaryTreeNode(val);
+    } else {
+      if(this.right) this.right.add(val);
+      else this.right = new BinaryTreeNode(val);
+    }
   }
   
   postOrderTraverse(cb) {
-    if (this.left){ this.left.postOrderTraverse(cb) };
-    if (this.right){  this.right.postOrderTraverse(cb) };
-    
-     cb(this.value); // cb called at end
-     // this returns undefined since there's no explicit return
+    if (this.left) this.left.postOrderTraverse(cb);
+    if (this.right) this.right.postOrderTraverse(cb);
+    cb(this.value);
   }
+  preOrderTraverse(cb) {
+    cb(this.value);
+    if (this.left) this.left.preOrderTraverse(cb);
+    if (this.right) this.right.preOrderTraverse(cb);
+  }
+  inOrderTraverse(cb) {
+    if (this.left) this.left.inOrderTraverse(cb);
+    cb(this.value);
+    if (this.right) this.right.inOrderTraverse(cb);
+  }
+
 }
 
 class BinaryTree {
@@ -122,41 +264,50 @@ class BinaryTree {
     this.root = new BinaryTreeNode();
   }
   
-  breadthFirst(cb) {
-    list = new LinkedList;
-    let q = new Queue(list);
-    q.enqueue(this.root);
+  //should add to left if lower, to right if higher
+  add(val) {
+
+    current = this.root;
     
+    while ( !current.left && current.right ) {
+      
+      if (val <= current) {
+        if (current.left) {
+          current=current.left
+        } else {
+          current.left = BinaryTreeNode(val);
+        }
+      }
+      
+      
+      if (val > current.right) {
+        
+      }
+    }
+    
+    
+  }
+  
+  breadthFirstTraversal(cb) {
+    let q = new Queue( new LinkedList);
+    q.enqueue(this.root);
     while(!q.isEmpty()) {
       let n = q.dequeue();
-      cb(n);
+      cb(n.value);
       if (n.left) q.enqueue(n.left);
       if (n.right) q.enqueue(n.right);
     }
-  }// breadthFirst
-}// BinaryTree
+  }
+}
 
 let b = new BinaryTree();
 
-b.root.value = 11;
+b.root.value = "root";
+b.root.left = new BinaryTreeNode("left");
+b.root.right = new BinaryTreeNode("right");
+b.root.left.left = new BinaryTreeNode("left left");
+b.root.left.right = new BinaryTreeNode("left right");
+b.root.right.left = new BinaryTreeNode("right left");
+b.root.right.right = new BinaryTreeNode("right right");
 
-///////////
-// Manually set nodes
-//////////
-
-b.root.left = new BinaryTreeNode(98);
-b.root.right = new BinaryTreeNode(77);
-
-b.root.left.right = new BinaryTreeNode(1);
-b.root.left.left = new BinaryTreeNode(9);
-
-b.root.left.left.left = new BinaryTreeNode(32398);
-b.root.left.left.right = new BinaryTreeNode(32334234298);
-
-
-
-// console.log(b.root);
-
-// b.root.preOrderTraverse(console.log);
-
-b.breadthFirst(console.log);
+b.breadthFirstTraversal(console.log);
