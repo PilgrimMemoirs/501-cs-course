d4e011d4a3615fc252b2419e4ad763fd9105ef04
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2018-12-13 11:17:42 -0800

    start binary tree

---
+++ binary_tree.js
@@ -0,0 +1,15 @@
+use 'strict';
+
+class BinaryTreeNode() {
+  constructor(val) {
+    this.left = null;
+    this.right = null;
+    this.value = val;
+  }
+}
+
+class BinaryTree() {
+  constructor() {
+    let root = new BinaryTreeNode();
+  }
+}
