8302d105f824e4aea3b43c19c2a5e45c512fe801
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2018-12-14 11:31:33 -0800

    outline knapsack problem

---
+++ knapsack.js
@@ -0,0 +1,28 @@
+// uses cost / weight effectiveness
+
+// n items
+  // weight: w1, w2, w3, w4
+  // vaue: v1, v2, v3, v4
+  
+  
+//4 stealable elements: 
+  //e1: w2 & v12
+  //e2: w1 & v10
+  //e3: w3 & v20
+  //e4: w2 & v15
+
+// Capacity of bag: 5
+
+// Possible solutions
+    //e3, e4
+      //w5  v35
+      
+    //e1, e2, e4 
+      //w5  v39
+      
+      
+      
+// Time Complexity: O(n*c) with c=capacity 
+// Space Complexity: O()
+  
+  // make a working solution. Benchmark it. 
